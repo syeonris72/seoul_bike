3476789b029246f8e2116ae0b1adf9ae145e0392
syeonris72 <syeonris72@gmail.com>
2026-07-17 18:03:37 +0900

    Update

--- data/reset_db.py
+++ data/reset_db.py
@@ -19,9 +19,7 @@ from sqlalchemy import MetaData  # Base 대신 MetaData 임포트
 # 메인 실행 블록
 # ==========================================
 if __name__ == "__main__":
-    # 사용자 입력을 통한 삭제 여부 강력 재확인
     confirm = input("[경고] DataBase 전체 Table 강제 삭제 (y/n): ").strip().lower()
-
     if confirm == 'y':
         meta = MetaData()
         meta.reflect(bind=engine)
