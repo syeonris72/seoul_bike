1c47a7250800039b6ea80dffac24404ff9e420cb
syeonris72 <syeonris72@gmail.com>
2026-07-17 18:03:09 +0900

    Update

--- data/reset_db.py
+++ data/reset_db.py
@@ -7,21 +7,26 @@ import sys
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 
+
 # ==========================================
 # 데이터베이스 모듈 임포트
 # ==========================================
 from database.db_connection import engine
-from models.models import Base
+from sqlalchemy import MetaData  # Base 대신 MetaData 임포트
+
 
 # ==========================================
-# 메인 실행 블록 (전체 테이블 삭제)
+# 메인 실행 블록
 # ==========================================
 if __name__ == "__main__":
-    # 사용자 입력을 통한 삭제 여부 재확인
-    confirm = input("\nDataBase 전체 Table 삭제 (y/n): ").strip().lower()
+    # 사용자 입력을 통한 삭제 여부 강력 재확인
+    confirm = input("[경고] DataBase 전체 Table 강제 삭제 (y/n): ").strip().lower()
 
     if confirm == 'y':
-        Base.metadata.drop_all(bind=engine)
-        print("========== 전체 테이블 삭제 완료 ==========")
+        meta = MetaData()
+        meta.reflect(bind=engine)
+        meta.drop_all(bind=engine)
+
+        print("========== 전체 테이블 강제 삭제 완료 ==========")
     else:
         print("\n========== 작업 취소 완료 ==========")
